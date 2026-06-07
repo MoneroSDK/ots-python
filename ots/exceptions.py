8c8f756a6d7fd53b097f8053faf2d6f76acecae2
who asks? <user@localhost>
2026-06-06 19:02:42 -0600

    removed stray print debug statements

--- ots/exceptions.py
+++ ots/exceptions.py
@@ -9,9 +9,6 @@ def exception_from_result(result: ots_result_t | _CDataBase) -> 'OtsException':
     """
     assert isinstance(result, (ots_result_t, _CDataBase)), "result must be an instance of ots_result_t or _CDataBase"
     assert ffi.typeof(_unwrap(result)) == ffi.typeof('ots_result_t*'), "result must be of type ots_result_t *"
-    print(type(result))
-    print(type(_unwrap(result)))
-    print(type(_unwrap(result).error))
     error: _CDataBase = _unwrap(result).error
     error_code: int = error.code
     if error_code in EXCEPTION_MAPPING:
