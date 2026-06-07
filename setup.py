3abab439f06f828340cb8139e6b6345f7f7d0492
who asks? <user@localhost>
2026-06-06 18:58:41 -0600

    bumb version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 from setuptools import setup, find_packages
 
 
-VERSION = '0.3.0'
+VERSION = '0.4.0'
 environ['PYTHONHASHSEED'] = sha256(f'Monero OTS {VERSION}'.encode('utf-8')).hexdigest()
 
 
